2b4c29b3fb625961215832a79474238525200949
Kenton Wang <smartepsh@gmail.com>
2024-02-18 13:11:14 +0800

    feat: fetch field

--- README.md
+++ README.md
@@ -4,6 +4,14 @@ CommandLine Tool to find citekey from bibtex file by file path.
 
 ## Usage
 
+### fetch-citekey  by field
+
 ```bash
-$ bibtex_org fetch-citekey --bibtex <bibtex file path> --key <the key you find> --value <the value you find>
+$ bibtex_org fetch-citekey --bibtex <bibtex file path> --key <the field key you find> --value <the value you find>
+```
+
+### fetch-field  by citekey
+
+```bash
+$ bibtex_org fetch-field --bibtex <bibtex file path> --citekey <the citekey you find> --key <the field key you find>
 ```
--- main.go
+++ main.go
@@ -11,9 +11,14 @@ import (
 var cli struct {
 	FetchCitekey struct {
 		Bibtex string `help:"bibtex file path" type:"existingfile" short:"b" required:""`
-		Key string `help:"entry key you find" optional:"" short:"k"`
-		Value string `help:"file path for the entry you want" optional:"" type:"existingfile" short:"v"`
-	} `cmd:"0" help:"Fetch citekey from bibtex file"`
+		Key string `help:"entry key you find" required:"" short:"k"`
+		Value string `help:"file path for the entry you want" required:"" type:"existingfile" short:"v"`
+	} `cmd:"0" help:"Fetch citekey from bibtex file by field key"`
+	FetchField struct {
+		Bibtex string `help:"bibtex file path" type:"existingfile" short:"b" required:""`
+		Citekey string `help:"entry key you find" required:"" short:"c"`
+		Key string `help:"entry field key you find" required:"" short:"k"`
+	} `cmd:"1" help:"Fetch field from bibtex file by citekey"`
 }
 
 func main() {
@@ -26,27 +31,47 @@ func main() {
 			Summary: true,
 		}))
 
-	file, ferr := os.Open(cli.FetchCitekey.Bibtex)
-	if ferr != nil {
-		os.Exit(1)
-	}
-	parsed, rerr := bibtex.Parse(file)
-	if rerr != nil {
-		os.Exit(1)
-	}
 
 	switch ctx.Command() {
 	case "fetch-citekey":
+		file, ferr := os.Open(cli.FetchCitekey.Bibtex)
+		if ferr != nil {
+			os.Exit(1)
+		}
+		parsed, rerr := bibtex.Parse(file)
+		if rerr != nil {
+			os.Exit(1)
+		}
 		for _, item := range parsed.Entries {
 			citekey := item.CiteName
 			value := item.Fields[cli.FetchCitekey.Key]
 			if value != nil {
 				if value.String() == cli.FetchCitekey.Value {
 					fmt.Println(citekey)
-					return
+					break
 				}
 			}
 		}
-		return
+
+	case "fetch-field":
+		file, ferr := os.Open(cli.FetchField.Bibtex)
+		if ferr != nil {
+			os.Exit(1)
+		}
+		parsed, rerr := bibtex.Parse(file)
+		if rerr != nil {
+			os.Exit(1)
+		}
+		for _, item := range parsed.Entries {
+			if item.CiteName != cli.FetchField.Citekey {
+				continue
+			}
+			value := item.Fields[cli.FetchField.Key]
+			if value != nil {
+				fmt.Println(value.String())
+				break
+			}
+		}
+		fmt.Println("")
 	}
 }
